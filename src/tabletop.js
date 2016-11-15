b003743420a8f4e7ba164701261c7016e89b8b19
Sebastianmol7 <sebastianmol7@gmail.com>
2016-11-15 10:46:39 -0500

    Update tabletop.js

--- src/tabletop.js
+++ src/tabletop.js
@@ -1,3 +1,19 @@
+<script type="text/javascript">
+  window.onload = function() { init() };
+
+  var public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE&output=html';
+
+  function init() {
+    Tabletop.init( { key: public_spreadsheet_url,
+                     callback: showInfo,
+                     simpleSheet: true } )
+  }
+
+  function showInfo(data, tabletop) {
+    alert("Successfully processed!")
+    console.log(data);
+  }
+</script>
 (function() {
   "use strict";
    var inNodeJS = false;
